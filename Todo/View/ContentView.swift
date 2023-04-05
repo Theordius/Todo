474caabf2f9a040d2573922620a813319822b8ab
Theordius <87307483+Theordius@users.noreply.github.com>
2023-04-05 12:13:38 +0200

    Change Form to VStack and add some animation to add item button.

--- Todo/View/ContentView.swift
+++ Todo/View/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     @FetchRequest(entity: Todo.entity(), sortDescriptors:  [NSSortDescriptor(keyPath: \Todo.name, ascending: true)]) var todos: FetchedResults<Todo>
     
     @State private var showingAddTodoView: Bool = false
+    @State private var animatingButton: Bool = false
     
     
     //MARK: - BODY
@@ -37,20 +38,56 @@ struct ContentView: View {
                 .navigationBarItems(
                     leading: EditButton(),
                     trailing:
-                    Button(action: {
-                    self.showingAddTodoView.toggle()
-                }) {
-                    Image(systemName: "plus")
-                } //: ADD BUTTON
-                    .sheet(isPresented: $showingAddTodoView) {
-                        AddTodoView().environment(\.managedObjectContext, self.viewContext)
-                    }
-            )
+                        Button(action: {
+                            self.showingAddTodoView.toggle()
+                        }) {
+                            Image(systemName: "plus")
+                        } //: ADD BUTTON
+                        .sheet(isPresented: $showingAddTodoView) {
+                            AddTodoView().environment(\.managedObjectContext, self.viewContext)
+                        }
+                )
                 //MARK: - NO TODO ITEMS
                 if todos.count == 0 {
-                   EmptyListView()
+                    EmptyListView()
                 }
             } //: ZSTACK
+            .sheet(isPresented: $showingAddTodoView) {
+                AddTodoView().environment(\.managedObjectContext, self.viewContext)
+            }
+            .overlay(
+                ZStack {
+                    Group {
+                        Circle()
+                            .fill(Color.blue)
+                            .opacity(self.animatingButton ? 0.2 : 0)
+                            .scaleEffect(self.animatingButton ? 1 : 0)
+                            .frame(width: 68, height: 68, alignment: .center)
+                        Circle()
+                            .fill(Color.blue)
+                            .opacity(self.animatingButton ? 0.15 : 0)
+                            .scaleEffect(self.animatingButton ? 1 : 0)
+                            .frame(width: 88, height: 88, alignment: .center)
+                    }
+                    .animation(Animation.easeInOut(duration: 2).repeatForever(autoreverses: true))
+                    
+                    Button(action: {
+                        self.showingAddTodoView.toggle()
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .background(Circle().fill(Color("ColorBase")))
+                            .frame(width: 48, height: 48, alignment: .center)
+                    } //: BUTTON
+                    .onAppear(perform: {
+                        self.animatingButton.toggle()
+                    })
+                } //: ZSTACK
+                    .padding(.bottom, 15)
+                    .padding(.trailing, 15)
+                ,alignment: .bottomTrailing
+            )
         } //: NAVIGATION
     }
     //MARK: - FUNCTION
